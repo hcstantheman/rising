d2c7258fd480556b8b67e0e233d4599dd75c665f
hcstantheman <62427735+hcstantheman@users.noreply.github.com>
2023-12-05 11:18:34 +0900

    roughly done with the footer. fix the fonts

--- components/footer/Footer.tsx
+++ components/footer/Footer.tsx
@@ -20,6 +20,55 @@ const Footer = () => {
             <div className={styles.address}>FAX番号083-974-5021</div>
             <div className={styles.address}>住所〒754-0002 山口県山口市小郡下郷842-9</div>
           </div>
+
+        </div>
+        <div className={styles.columnsContainer}>
+          <div className={styles.firstColumn}>
+            <p className={styles.title}>ライジンググループのお店作り</p>
+            <ul className={styles.list}>
+              <li className={styles.listItem}>調剤薬局の特徴</li>
+              <li className={styles.listItem}>お店作りについて</li>
+              <li className={styles.listItem}>健康サポート薬局について</li>
+              <li className={styles.listItem}>災害発生時の体制</li>
+            </ul>
+            <p className={styles.secondTitle}>処方箋受付について</p>
+            <ul className={styles.list}>
+              <li className={styles.listItem}>インターネットからの処方箋受付</li>
+              <li className={styles.listItem}>LINEでの処方箋受付について</li>
+            </ul>
+          </div>
+          <div className={styles.secondColumn}>
+            <p className={styles.title}>薬剤師について</p>
+            <ul className={styles.list}>
+              <li className={styles.listItem}>薬剤師教育について</li>
+              <li className={styles.listItem}>薬剤師　在宅訪問</li>
+            </ul>
+            <p className={styles.secondTitle}>その他</p>
+            <ul className={styles.list}>
+              <li className={styles.listItem}>BLOG</li>
+              <li className={styles.listItem}>ジェネリック医薬品について</li>
+              <li className={styles.listItem}>プライバシーポリシー</li>
+            </ul>
+          </div>
+          <div className={styles.thirdColumn}>
+            <p className={styles.title}>薬局一覧</p>
+            <ul className={styles.doubleList}>
+              <li className={styles.listItem}>まさむね薬局</li>
+              <li className={styles.listItem}>ホワイト薬局</li>
+              <li className={styles.listItem}>秀栄薬局</li>
+              <li className={styles.listItem}>あいお薬局</li>
+              <li className={styles.listItem}>ソレイユ薬局</li>
+              <li className={styles.listItem}>美秋薬局</li>
+              <li className={styles.listItem}>セレッソ薬局</li>
+              <li className={styles.listItem}>ミント薬局</li>
+              <li className={styles.listItem}>厚南薬局</li>
+              <li className={styles.listItem}>サン薬局</li>
+              <li className={styles.listItem}>ぐうです薬局</li>
+              <li className={styles.listItem}>川棚薬局</li>
+              <li className={styles.listItem}>関門薬局</li>
+              <li className={styles.listItem}>はすの実薬局</li>
+            </ul>
+          </div>
         </div>
       </div>
     </div>
