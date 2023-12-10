bd8663e5d23ab2244287ac1c318186c4c9710ea4
hcstantheman <62427735+hcstantheman@users.noreply.github.com>
2023-12-10 18:56:49 +0900

    done with the first part at the line section

---
+++ pages/line.tsx
@@ -0,0 +1,29 @@
+import React from 'react';
+import Header from '../components/header/Header';
+import Footer from '../components/footer/Footer';
+import styles from './css/line.module.scss';
+
+const Line: React.FC = () => {
+  return (
+    <div>
+      <Header text='LINEでの処方箋受付' />
+      <div className={styles.intro}>
+        <div className={styles.introText}>
+          ライジンググループ各店舗のLINE公式アカウントにご登録いただくと、下記のサービスをご利用いただけます。
+          感染症予防にも有効なサービスですので、是非ご利用ください。
+        </div>
+        <div className={styles.introButton}>
+          <div className={styles.layer}></div>
+          <div className={styles.topLayer}>
+            <span className={styles.buttonText}>店舗一覧へ ＞</span>
+          </div>
+        </div>
+
+
+      </div>
+      <Footer />
+    </div>
+  );
+};
+
+export default Line;
