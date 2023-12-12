d977ce733c3ae7b9ac3132c63e3bd392df05716a
hcstantheman <62427735+hcstantheman@users.noreply.github.com>
2023-12-12 16:57:48 +0900

    done with the first half

--- pages/generic.tsx
+++ pages/generic.tsx
@@ -62,6 +62,58 @@ const Generic: React.FC = () => {
       </div>
       <div className={styles.newDrugContainer}>
         <div className={styles.title}>新薬とジェネリック医薬品の成分について</div>
+        <p className={styles.contentTitle}>同じ成分</p>
+        <div className={styles.firstRow}>
+          <div className={styles.circle}>
+            <p>先発医薬品</p>
+          </div>
+          <Image
+            src="/img/generic-arrow-4.svg"
+            width={140} height={10}
+            alt="arrow"/>
+          <div className={styles.shorterItem}>
+            <p className={styles.itemFirstLine}>主薬</p>
+            <p className={styles.itemSecondLine}>薬の主成分</p>
+          </div>
+          <Image
+            src="/img/generic-arrow-4.svg"
+            width={140} height={10}
+            alt="arrow" className={styles.arrow5}/>
+          <div className={styles.circle}>
+          </div>
+        </div>
+        <div className={styles.secondRow}>
+          <Image
+            src="/img/generic-arrow-5.png"
+            width={320} height={168}
+            alt="arrow"
+            className={styles.leftArrow}/>
+          <p>異なっていても良い成分</p>
+          <Image
+            src="/img/generic-arrow-5.png"
+            width={320} height={168}
+            alt="arrow" className={styles.rightArrow}/>
+        </div>
+        <div className={styles.giantItem}>
+          <div className={styles.textContainer}>
+            <div className={styles.textbox}>
+              <div className={styles.firstLine}>結合剤</div>
+              <div className={styles.secondLine}>硬さ・形状を保つ</div>
+            </div>
+            <div className={styles.textbox}>
+              <div className={styles.firstLine}>崩壊剤</div>
+              <div className={styles.secondLine}>崩壊を促進する</div>
+            </div>
+            <div className={styles.textbox}>
+              <div className={styles.firstLine}>賊形剤</div>
+              <div className={styles.secondLine}>大きさ・質量を与える</div>
+            </div>
+            <div className={styles.textbox}>
+              <div className={styles.firstLine}>潤沢剤</div>
+              <div className={styles.secondLine}>圧縮伝達性を良くする</div>
+            </div>
+          </div>
+        </div>
       </div>
       <Footer />
     </div>
