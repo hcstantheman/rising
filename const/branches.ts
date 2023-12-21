5ef99e0dabfd9f03c742b66ebca16a132e29a304
gsc <hcstanleychau@gmail.com>
2023-12-22 01:57:34 +0900

    create the const for the branches

---
+++ const/branches.ts
@@ -0,0 +1,20 @@
+import BlogMenuProps from "../type/BlogMenuProps";
+
+const branchesArr:BlogMenuProps[] = [
+  { value: '1', label: 'まさむね薬局' },
+  { value: '2', label: 'ホワイト薬局' },
+  { value: '3', label: '秀英薬局' },
+  { value: '4', label: 'あいお薬局' },
+  { value: '5', label: 'ソレイユ薬局' },
+  { value: '6', label: '美秋薬局' },
+  { value: '7', label: 'セレッソ薬局' },
+  { value: '8', label: 'ミント薬局' },
+  { value: '9', label: '厚南薬局' },
+  { value: '10', label: 'サン薬局' },
+  { value: '11', label: 'ぐうです薬局' },
+  { value: '12', label: '川棚薬局' },
+  { value: '13', label: '関門薬局' },
+  { value: '14', label: 'はすの実薬局' }
+]
+
+export default branchesArr;
---
+++ type/BlogMenuProps.ts
@@ -0,0 +1,6 @@
+interface BlogMenuProps {
+  value: string;
+  label: string;
+}
+
+export default BlogMenuProps;
