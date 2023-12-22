585b9cb82f6250fcdf56248af8ea8df6a2b72491
gsc <hcstanleychau@gmail.com>
2023-12-22 15:34:57 +0900

    done with the pagination

--- api/blogApi.ts
+++ api/blogApi.ts
@@ -2,14 +2,18 @@ import axios from 'axios';
 
 const BASE_URL = 'https://rising.local/wp-json/wp/v2';
 
-export const fetchPosts = async (tagId?: string, itemsPerPage: number = 10) => {
-  let url = `${BASE_URL}/posts?_embed&per_page=${itemsPerPage}`;
+export const fetchPosts = async (page: number = 1, tagId?: string, itemsPerPage: number = 9) => {
+  let url = `${BASE_URL}/posts?_embed&per_page=${itemsPerPage}&page=${page}`;
+  console.log('url', url);
   if (tagId) {
     url += `&tags=${tagId}`;  // Use the 'tags' query parameter to filter by tag ID
   }
   const response = await axios.get(url);
-  return response.data;
+  const totalPosts = parseInt(response.headers['x-wp-total']);
+
+  return { posts: response.data, totalPosts };
 };
+
 
 export const fetchTags = async (postId: any) => {
   const response = await axios.get(`${BASE_URL}/tags?post=${postId}`);
--- pages/blog.tsx
+++ pages/blog.tsx
@@ -15,20 +15,27 @@ const Blog: React.FC = () => {
   const [posts, setPosts] = useState<any[]>([]); // State to hold your posts
   const itemsPerPage = 9;
   const [selectedBranch, setSelectedBranch] = useState<{ value: string; label: string } | null>(null);
+  const [pageCount, setPageCount] = useState<number>(0);
+
+  const handlePageClick  = (data: { selected: number }) => {
+    const selectedPage = data.selected;
+    setCurrentPage(selectedPage);
+  };
   const handleFilterChange = (selectedOption: { value: string; label: string } | null) => {
     setSelectedBranch(selectedOption);
   };
   useEffect(() => {
     const fetchData = async () => {
-      const posts = await fetchPosts(selectedBranch?.value, itemsPerPage);
+      const { posts, totalPosts } = await fetchPosts(currentPage + 1, selectedBranch?.value, itemsPerPage);
       // @ts-ignore
       const postsWithData = await Promise.all(posts.map(async post => {
         const imageUrl = ExtractImageUrl(post.content.rendered) || '';
         const tags = await fetchTags(post.id);
-
+        const totalPages = Math.ceil(totalPosts / itemsPerPage);
+        setPageCount(totalPages);
         return { ...post, imageUrl, tags };
       }));
-      console.log(postsWithData)
+
       setPosts(postsWithData);
     };
     fetchData();
@@ -62,10 +69,8 @@ const Blog: React.FC = () => {
       ...fontStyle,
     }),
     indicatorSeparator: () => ({
-      display: 'none', // Hide the indicator separator
+      display: 'none',
     })
-
-
   };
 
   return(
@@ -94,15 +99,17 @@ const Blog: React.FC = () => {
         </div>
       </div>
       <ReactPaginate
-        previousLabel={'Previous'}
-        nextLabel={'Next'}
+        previousClassName={currentPage === 0 ? styles.hidden : ""}
+        nextClassName={currentPage === pageCount - 1 ? styles.hidden : ""}
+        previousLabel={currentPage === 0 ? null : '←'}
+        nextLabel={currentPage === pageCount - 1 ? null : '→'}
         breakLabel={'...'}
         pageCount={pageCount} // Total number of pages
         marginPagesDisplayed={2} // Number of pages to display at the edges
         pageRangeDisplayed={5} // Number of pages to display in the middle
         onPageChange={handlePageClick} // Function to call when a page is clicked
-        containerClassName={'pagination'} // Class name for the container
-        activeClassName={'active'} // Class name for the active page
+        containerClassName={styles.pagination} // Class name for the container
+        activeClassName={styles.active} // Class name for the active page
       />
       <Footer />
     </div>
