1207051b3ad4db35b97163d03beb3d39547f124b
gsc <hcstanleychau@gmail.com>
2023-12-24 04:58:37 +0900

    update button that support blue color

--- components/common/StyleButton.tsx
+++ components/common/StyleButton.tsx
@@ -5,16 +5,24 @@ import Link from "next/link";
 interface ButtonProps {
   text: string;
   to: string;
+  blue?: boolean;
 }
 
-const StyleButton: React.FC<ButtonProps> = ({text, to}) => {
-
+const StyleButton: React.FC<ButtonProps> = ({text, to, blue}) => {
+  let buttonStyle = {}
+  let topLayerStyle = {}
+  let textStyle = {}
+  if (blue){
+    buttonStyle = {borderColor: '#5EAED2'}
+    topLayerStyle = {backgroundColor: '#5EAED2'}
+    textStyle = {color: '#FFFFFF'}
+  }
   return (
     <Link href={to} passHref>
       <div className={styles.button}>
-        <div className={styles.layer}></div>
-        <div className={styles.topLayer}>
-          <span className={styles.buttonText}>{text}</span>
+        <div className={styles.layer} style={buttonStyle}></div>
+        <div className={styles.topLayer} style={topLayerStyle}>
+          <span className={styles.buttonText} style={textStyle}>{text}</span>
         </div>
       </div>
     </Link>
