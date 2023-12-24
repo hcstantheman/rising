3e4253ccb916e05a76adcbbe8080ab6ea7e2b523
hcstantheman <62427735+hcstantheman@users.noreply.github.com>
2023-12-24 23:31:45 +0900

    update the code so far

---
+++ components/index/Location.tsx
@@ -0,0 +1,29 @@
+// IndexLocation.tsx
+import React from "react";
+import styles from './css/location.module.scss';
+import Image from "next/image";
+import pinLocations from "../../const/location"
+
+const IndexLocation: React.FC = () => {
+  console.log(pinLocations)
+  return (
+    <div className={styles.container}>
+      <p className={styles.title}>店舗所在</p>
+      <div className={styles.map}>
+        <Image src={'/img/index-location-bg.png'} alt={'location-bg'} width={1296} height={828} className={styles.bg}/>
+        {pinLocations.map((pin, index) => (
+          <div key={index} className={styles.pin} style={{
+            position: 'absolute',
+            top: `${pin.top * 100}%`,
+            left: `${pin.left * 100}%`,
+            transform: 'translate(-50%, -50%)' // Adjust if necessary
+          }}>
+            <Image src={'/img/index-location-inactive.svg'} alt={`${pin.name} location`} width={60} height={60}/>
+          </div>
+        ))}
+      </div>
+    </div>
+  )
+}
+
+export default IndexLocation;
