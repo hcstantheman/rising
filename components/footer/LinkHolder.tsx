e70d43caa2c4df580bb59d500e07177335120c46
hcstantheman <62427735+hcstantheman@users.noreply.github.com>
2024-01-11 13:53:12 +0900

    done with the link

--- components/footer/LinkHolder.tsx
+++ components/footer/LinkHolder.tsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, {useState} from 'react';
 import Link from 'next/link';
 import styles from './css/LinkHolder.module.scss';
 
@@ -12,30 +12,24 @@ interface Props {
 }
 
 const LinkHolder: React.FC<Props> = ({ title, links, twoColumn}) => {
+  const [isOpen, setIsOpen] = useState(false);
+  const toggleDropdown = () => setIsOpen(!isOpen);
+
+  const baseListClassName = twoColumn ? styles.doubleList : styles.list;
+  const finalListClassName = isOpen ? styles.listOpen : baseListClassName;
+
   return (
     <div className={styles.container}>
-      <p className={styles.title}>{title}</p>
-      { !twoColumn?
-        <ul className={styles.list}>
-          {links.map((link, index) => (
-            <li key={index} className={styles.listItem}>
-              <Link href={link.href} className={styles.link}>
-                {link.name}
-              </Link>
-            </li>
-          ))}
-        </ul>
-        :
-        <ul className={styles.doubleList}>
-          {links.map((link, index) => (
-            <li key={index} className={styles.listItem}>
-              <Link href={link.href} className={styles.link}>
-                {link.name}
-              </Link>
-            </li>
-          ))}
-        </ul>
-      }
+      <p className={styles.title} onClick={toggleDropdown}>{title}</p>
+      <ul className={finalListClassName}>
+        {links.map((link, index) => (
+          <li key={index} className={styles.listItem}>
+            <Link href={link.href} className={styles.link}>
+              {link.name}
+            </Link>
+          </li>
+        ))}
+      </ul>
     </div>
   );
 };
