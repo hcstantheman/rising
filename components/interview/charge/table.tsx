c27cc99236b2736818c21edd490fc15592f9a0c9
hcstantheman <62427735+hcstantheman@users.noreply.github.com>
2024-01-15 19:27:38 +0900

    error in table

--- components/interview/charge/table.tsx
+++ components/interview/charge/table.tsx
@@ -4,18 +4,22 @@ import styles from "./css/table.module.scss";
 const PriceTable: React.FC = () => {
   return(
     <table className={styles.table}>
-      <tr className={styles.firstRow}>
-        <td className={styles.firstColumn}></td>
-        <td>単一建物住居者１人</td>
-        <td>単一建物住居者２～９人</td>
-        <td>単一建物住居者１０人以上</td>
-      </tr>
-      <tr className={styles.secondRow}>
-        <td className={styles.firstColumn}>１割負担の方</td>
-        <td>５１７円</td>
-        <td>３７８円</td>
-        <td>３４１円</td>
-      </tr>
+      <tbody>
+        <tr className={styles.firstRow}>
+          <td className={styles.firstColumn}></td>
+          <td>単一建物住居者１人</td>
+          <td>単一建物住居者２～９人</td>
+          <td>単一建物住居者１０人以上</td>
+        </tr>
+      </tbody>
+      <tbody>
+        <tr className={styles.secondRow}>
+          <td className={styles.firstColumn}>１割負担の方</td>
+          <td>５１７円</td>
+          <td>３７８円</td>
+          <td>３４１円</td>
+        </tr>
+      </tbody>
     </table>
   )
 }
