30de0a8442fbeb76ad157c1453239138108d7233
hcstantheman <62427735+hcstantheman@users.noreply.github.com>
2024-01-16 12:06:28 +0900

    done with price intro. work on the table in a bit

--- components/interview/charge/price.tsx
+++ components/interview/charge/price.tsx
@@ -1,10 +1,19 @@
-import React from "react";
+import React, {useEffect, useState} from "react";
 import styles from "./css/price.module.scss";
 import DottedLine from "../../common/DottedLine";
 import PriceTable from "./table";
 import PriceNote from "./note";
 
 const Price: React.FC = () => {
+  const [numDots, setNumDots] = useState(0);
+  useEffect(() => {
+    const handleResize = () => {
+      setNumDots(window.innerWidth <= 860 ? 38 : 66);
+    };
+    handleResize();
+    window.addEventListener('resize', handleResize);
+    return () => window.removeEventListener('resize', handleResize);
+  }, []);
   return(
     <div className={styles.container}>
       <div className={styles.titleContainer}>
@@ -12,7 +21,7 @@ const Price: React.FC = () => {
           <p className={styles.title}>訪問1回あたりのご利用料金</p>
           <div className={styles.button}>月4回まで</div>
         </div>
-        <DottedLine numDots={66} wider={true}/>
+        <DottedLine numDots={numDots} wider={true}/>
       </div>
       <div className={styles.tableContainer}>
         <PriceTable/>
