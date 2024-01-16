683b33953f6a49d1ff1fb0041f91e3e52f0015e3
hcstantheman <62427735+hcstantheman@users.noreply.github.com>
2024-01-16 10:15:25 +0900

    done with interview intro

--- pages/interview.tsx
+++ pages/interview.tsx
@@ -15,14 +15,14 @@ const Interview: React.FC = () => {
         <div className={styles.intro}>
           <Intro/>
         </div>
-        <div className={styles.content}>
-          <Content/>
-        </div>
+        {/*<div className={styles.content}>*/}
+        {/*  <Content/>*/}
+        {/*</div>*/}
       </div>
-      <div className={styles.discussion}>
-        <Discussion/>
-      </div>
-      <Charge/>
+      {/*<div className={styles.discussion}>*/}
+      {/*  <Discussion/>*/}
+      {/*</div>*/}
+      {/*<Charge/>*/}
       <Footer/>
     </div>
   )
